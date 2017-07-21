27e117842d8d268870122e0a62e70b80251942ab
hanlinsan <hanlinsan@gmail.com>
2017-07-21 16:12:03 +0800

    SH is done

--- sh.py
+++ sh.py
@@ -8,10 +8,14 @@ Created on Thu Jul 20 16:17:16 2017
 
 import pandas as pd
 
-frame = pd.read_csv('./SH0.csv')
+frame = pd.read_csv('D:/StockListA/SH00.csv')
 print(frame.columns)
 
 
-#with open('./SH.csv') as f :
-#    for line in f.readlines():
-#        print(line)
+with open('./SH.csv') as f :
+    for line in f.readlines():
+        print(line)
+
+
+d = {'one':pd.Series([1,2,3]), 'two':pd.Series([4,5,6])}
+print(d['one'])
